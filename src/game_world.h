c135cebdebbc87d6dbc7f2d9c3e904eea99b4f2c
Grant J. Butler <gjb7@njit.edu>
2014-09-20 10:33:49 -0400

    Add game_world.

---
+++ src/game_world.h
@@ -0,0 +1,21 @@
+//
+//  game_world.h
+//  zelda
+//
+//  Created by Grant Butler on 9/20/14.
+//  Copyright (c) 2014 Grant Butler. All rights reserved.
+//
+
+#ifndef __zelda__game_world__
+#define __zelda__game_world__
+
+#include <stdio.h>
+#include "entity.h"
+
+typedef struct game_world_s {
+    
+} game_world_t;
+
+entity_t *game_world_create();
+
+#endif /* defined(__zelda__game_world__) */
