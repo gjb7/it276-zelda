d587322bee7991aa5b09f788f566052f5f12dc07
Grant J. Butler <gjb7@njit.edu>
2014-09-23 18:44:02 -0400

    Add simple window test.

--- src/main.c
+++ src/main.c
@@ -1,4 +1,7 @@
 #include <SDL2/SDL.h>
+#include <stdlib.h>
+
+#include "window.h"
 
 #ifdef TESTS
 #include <check.h>
@@ -18,7 +21,18 @@ int main(int argc, char **argv) {
     srunner_free(runner);
     return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 #else
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+        fprintf(stderr, "Error initializing SDL: %s", SDL_GetError());
+        
+        return EXIT_FAILURE;
+    }
+    atexit(SDL_Quit);
     
+    window_t *window = window_create("Hello World!", 320, 480);
+    
+    SDL_Delay(2000);
+    
+    window_free(window);
 #endif
 	
 	return 0;
