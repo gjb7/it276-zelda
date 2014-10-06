4a74bd46971f27804b319cfa4f4fc64a7e74ceae
Grant J. Butler <gjb7@njit.edu>
2014-10-06 19:19:56 -0400

    Calculate viewport to get things offset correctly.

--- src/entity.c
+++ src/entity.c
@@ -116,6 +116,26 @@ void _entity_render_iterator(gpointer data, gpointer user_data) {
 void entity_render(entity_t *e) {
     assert(e != NULL);
     
+    SDL_Point rendererSize = graphics_global_renderer_size();
+    SDL_Rect viewportSize;
+    viewportSize.x = 0;
+    viewportSize.y = 0;
+    viewportSize.w = rendererSize.x;
+    viewportSize.h = rendererSize.y;
+    
+    if (e->parent) {
+        entity_t *parent = e->parent;
+        
+        do {
+            viewportSize.x += parent->position.x;
+            viewportSize.y += parent->position.y;
+            
+            parent = parent->parent;
+        } while(parent != NULL);
+    }
+    
+    SDL_RenderSetViewport(graphics_get_global_renderer(), &viewportSize);
+    
     if (e->render != NULL) {
         e->render(e);
     }
