0cb2cbd97196ff52444194403887965afa3f84c9
Grant J. Butler <gjb7@njit.edu>
2014-10-19 23:03:13 -0400

    Destroy _resource_table on exit.

--- src/resource.c
+++ src/resource.c
@@ -14,12 +14,19 @@
 
 static GHashTable *_resource_table = NULL;
 
+void _resource_quit(void);
 void _resource_free(resource_t *resource);
 
 bool resource_init(void) {
     _resource_table = g_hash_table_new_full(g_str_hash, g_str_equal, NULL, NULL);
     
+    atexit(_resource_quit);
+    
     return true;
+}
+
+void _resource_quit(void) {
+    g_hash_table_destroy(_resource_table);
 }
 
 resource_t *resource_load(char *file, resource_type type) {
