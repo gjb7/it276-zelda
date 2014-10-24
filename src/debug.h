879c7c67b8640f5386132c224a5879013a7f5c17
Grant J. Butler <gjb7@njit.edu>
2014-10-24 00:07:07 -0400

    Add debug option to draw collision boxes.

---
+++ src/debug.h
@@ -0,0 +1,18 @@
+//
+//  debug.h
+//  zelda
+//
+//  Created by Grant Butler on 10/23/14.
+//  Copyright (c) 2014 Grant Butler. All rights reserved.
+//
+
+#ifndef __zelda__debug__
+#define __zelda__debug__
+
+#include <stdio.h>
+#include <stdbool.h>
+
+void debug_set_render_collision_boxes(bool renderCollisionBoxes);
+bool debug_get_render_collision_boxes();
+
+#endif /* defined(__zelda__debug__) */
