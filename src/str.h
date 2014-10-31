1ec20d3b8645963990846cd8f760b737074c1de4
Grant J. Butler <gjb7@njit.edu>
2014-10-31 08:34:54 -0400

    Add string utilities.

---
+++ src/str.h
@@ -0,0 +1,17 @@
+/*
+//  str.h
+//  zelda
+//
+//  Created by Grant Butler on 10/29/14.
+//  Copyright (c) 2014 Grant Butler. All rights reserved.
+*/
+
+#ifndef __zelda__str__
+#define __zelda__str__
+
+#include <stdio.h>
+#include <stdbool.h>
+
+bool str_starts_with(char *string, const char *prefix);
+
+#endif /* defined(__zelda__str__) */
