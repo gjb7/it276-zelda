8f0ad0ce511909d955a2990b993e45ad420ddcc4
Grant J. Butler <gjb7@njit.edu>
2014-12-18 17:21:59 -0500

    Add inventory hud.

---
+++ src/inventory_hud.c
@@ -0,0 +1,20 @@
+//
+//  inventory_hud.c
+//  zelda
+//
+//  Created by Grant Butler on 12/18/14.
+//  Copyright (c) 2014 Grant Butler. All rights reserved.
+//
+
+#include "inventory_hud.h"
+
+entity_t *hud_create() {
+    entity_t *hud = entity_create();
+    if (!hud) {
+        return NULL;
+    }
+    
+    
+    
+    return hud;
+}
---
+++ src/inventory_hud.h
@@ -0,0 +1,21 @@
+//
+//  inventory_hud.h
+//  zelda
+//
+//  Created by Grant Butler on 12/18/14.
+//  Copyright (c) 2014 Grant Butler. All rights reserved.
+//
+
+#ifndef __zelda__inventory_hud__
+#define __zelda__inventory_hud__
+
+#include "entity.h"
+#include "sprite.h"
+
+typedef struct inventory_hud_s {
+    sprite_t *background_sprite;
+} inventory_hud_t;
+
+entity_t *hud_create();
+
+#endif /* defined(__zelda__inventory_hud__) */
